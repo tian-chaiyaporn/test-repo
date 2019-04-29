e19231bf5eed6736c85121ce000b7d215a86eeae
tian <tian_chaiya12@hotmail.com>
2019-04-29 22:17:02 +0700

    test deploy

--- artifacts/static-app.js
+++ artifacts/static-app.js
@@ -3263,7 +3263,7 @@ __webpack_require__.r(__webpack_exports__);
     style: {
       textAlign: 'center'
     }
-  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", null, "Welcome to React-Static"));
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", null, "Welcome to React-Static 1"));
 });
 
 /***/ }),
