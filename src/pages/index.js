c31aaad8edf59355eaa60197840f4e986e1cc0c9
tian <tian_chaiya12@hotmail.com>
2019-04-30 12:46:36 +0700

    test deploy

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default () => (
   <div style={{ textAlign: 'center' }}>
-    <h1>Welcome to React-Static 1</h1>
+    <h1>Welcome to React-Static 2</h1>
   </div>
 )
